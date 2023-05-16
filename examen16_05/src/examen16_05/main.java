14fcd644b213c546d754011ed8f425b97f3f0454
eltitogilito <jordirc40@gmail.com>
2023-05-16 10:08:44 +0200

    Primera subida del examen. He creado un syso.

---
+++ examen16_05/src/examen16_05/main.java
@@ -0,0 +1,10 @@
+package examen16_05;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("Como estan los máquinas");
+
+	}
+
+}
---
+++ examen16_05/src/module-info.java
@@ -0,0 +1,3 @@
+
+module examen16_05 {
+}
